364ae8ee759710120aa9f6698444e7205371a4c6
Dimon1989k <dima1989k@ukr.net>
2022-09-21 16:11:03 +0300

    finish 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,47 @@
 import { galleryItems } from "./gallery-items.js";
+const gallery = document.querySelector(".gallery");
+function createGalleryTemplate(picturesList) {
+  return picturesList
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join("");
+}
 
-// Change code below this line
+const galleryTemplate = createGalleryTemplate(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", galleryTemplate);
+gallery.addEventListener("click", handleClick);
 
-console.log(galleryItems);
+function handleClick(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  e.preventDefault();
+
+  const modalGallary = basicLightbox.create(
+    `<img src="${e.target.dataset.source}">`,
+    {
+      onShow: () => window.addEventListener("keydown", escapeCloseAlt),
+      onclose: () => window.removeEventListener("keydown", escapeCloseAlt),
+    }
+  );
+
+  modalGallary.show();
+  window.addEventListener("keydown", escapeCloseAlt.bind(modalGallary));
+}
+
+function escapeCloseAlt(e) {
+  if (e.code === "Escape") {
+    this.close();
+  }
+}
