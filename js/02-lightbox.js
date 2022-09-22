e5d4c88ed65ec9255010eea53112c337290694b1
Dimon1989k <dima1989k@ukr.net>
2022-09-22 11:21:45 +0300

    finish task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const createGalleryTemplate = ({ preview, original, description }) => `
+  <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+`;
+
+const getImagesGallery = (images) =>
+  images.map((item) => createGalleryTemplate(item)).join("");
+
+gallery.innerHTML = getImagesGallery(galleryItems);
+gallery.addEventListener("click", handleClick);
+
+new window.SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  captionPosition: "top",
+});
+
+function handleClick(e) {
+  e.preventDefault();
+}
